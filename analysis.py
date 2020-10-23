21c0e133930f80b179f83d3b83f765bb404a6780
Rahul <footybeast7@gmail.com>
2020-10-23 00:21:01 -0400

    makes a basic graph now, and general updates

--- analysis.py
+++ analysis.py
@@ -7,15 +7,43 @@ This module conducts analysis on the data, including
 
 import utils
 import pandas as pd
-import matplotlib as mpl
+import matplotlib.pyplot as plt
+import datetime
+
+month_names = {
+    1: "Jan",
+    2: "Feb",
+    3: "Mar",
+    4: "Apr",
+    5: "May",
+    6: "Jun",
+    7: "Jul",
+    8: "Aug",
+    9: "Sept",
+    10: "Oct",
+    11: "Nov",
+    12: "Dec",
+}
+
+
 
 class Analysis:
     def __init__(self, df):
         self.delta = 0
+        self.data = df
+        self.analysis_dict = dict()
 
-    def value_change(self, df):
+    def value_change(self):
         print("value change is:{}").format(self.delta)
         return self.delta
 
-    def make_graph(self, df):
+    def make_graph(self):
+        months = self.data['Month'].tolist()
+        years = self.data['Year'].tolist()
+        month_year = [str(months[i]) + "-" + str(years[i]) for i in range(0,len(months))]
+
+        values = self.data['Net Worth'].apply(int).tolist()
+
+        plt.plot(month_year, values)
+        plt.show()
         print("graph goes here")
--- make_report.py
+++ make_report.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import datetime
 import utils
+import analysis
 
 if __name__ == "__main__":
     print("Starting program")
@@ -10,6 +11,9 @@ if __name__ == "__main__":
     df = utils.xlsx_to_df()
     curr_month = utils.curr_month_list(df)
     last_month = utils.last_month_list(df)
+
+    analysis = analysis.Analysis(df)
+    analysis.make_graph()
 
 
     print("Report has been created and sent")
